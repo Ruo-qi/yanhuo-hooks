ae2681075b3895f3e34c48a57aedc5487337a28a
若琦 <2674675520@qq.com>
2024-02-05 12:41:09 +0800

    perf: 规范化

--- packages/hooks/webpack.config.js
+++ packages/hooks/webpack.config.js
@@ -5,8 +5,8 @@ const path = require('path');
 module.exports = merge(common, {
   entry: './es/index.js',
   output: {
-    filename: 'yanHuoHooks.js',
-    library: 'yanHuoHooks',
+    filename: 'yanhuoHooks.js',
+    library: 'yanhuoHooks',
     path: path.resolve(__dirname, './dist'),
   },
 });
